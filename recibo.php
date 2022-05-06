223c2a292c6f3c5ed09148231cd6f54543e78626
Francisco <fg.14.cavalcandido@gmail.com>
2022-05-06 20:55:05 -0300

    francisco commit

--- recibo.php
+++ recibo.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<link rel="stylesheet" href="./style.css">
+<link rel="recibosheet" href="./recibo.css">
 
 <body>
 
@@ -18,9 +18,21 @@
     $residente = $_POST['inquilino'];
     $cpfCnpjInquilino = $_POST['cpfCnpj'];
     $proprietario = $_POST['propi'];
-    
-
-
+    $cpfCnpjProprietario = $_POST['cpfCnpjPro'];
+    $rua = $_POST['ruaImovel'];
+    $complementoI = $_POST['complementoInput'];
+    $num = $_POST['numeroImovel'];
+    $bairroImovel = $_POST['bairro'];
+    $cidade = $_POST['cidadeImovel'];
+    $estado = $_POST['estadoImovel'];
     ?>
+
+    <?php
+    echo "Recebemos do Inquilino " .$residente."<br>"; 
+    echo "O pagamento do aluguel de um(a), " .$complementoI. "<br>";
+    echo "No valor de ".$valor. "<br>";
+    echo "Referente ao mes de " .$mes. "<br>";
+    echo "Ano " .$ano. "<br>";
+    ?>
+
 </body>
-    
